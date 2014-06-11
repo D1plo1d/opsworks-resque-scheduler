79d741490150104bfaa61fea270002520a23a46a
D1plo1d <d1plo1d@d1plo1d.com>
2014-06-11 14:06:11 -0400

    Sane defaults for path.

--- attributes/configure.rb
+++ attributes/configure.rb
@@ -1,2 +1,2 @@
-default['resque-scheduler']['path'] = "/srv/www/mailee_staging/current"
+default['resque-scheduler']['path'] = "#{node[:deploy].values[0][:deploy_to]}/current"
 default['resque-scheduler']['rails_env'] = "development"
